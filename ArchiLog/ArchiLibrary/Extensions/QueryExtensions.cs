e799c495d54f38900e93807a3ed1a28225b65f2f
ElodieGRR <97110472+ElodieGRR@users.noreply.github.com>
2022-11-03 12:28:25 +0100

    fonction desc pour le tri

--- ArchiLog/ArchiLibrary/Extensions/QueryExtensions.cs
+++ ArchiLog/ArchiLibrary/Extensions/QueryExtensions.cs
@@ -13,6 +13,7 @@ namespace ArchiLibrary.Extensions
 
         public static IOrderedQueryable<TModel> Sort<TModel>(this IQueryable<TModel> query, Params p)
         {
+            //condition pour la fonction ascendante
             if (!string.IsNullOrWhiteSpace(p.Asc))
             {
                 string champ = p.Asc;
@@ -26,6 +27,22 @@ namespace ArchiLibrary.Extensions
 
                 //utilisation lambda
                 return query.OrderBy(lambda);
+
+            } 
+            //condition pour la fonction descendante
+            else if (!string.IsNullOrWhiteSpace(p.Desc))
+            {
+                string champ = p.Desc;
+
+                //créer lambda
+                var parameter = Expression.Parameter(typeof(TModel), "x");
+                var property = Expression.Property(parameter, champ/*"Name"*/);
+
+                var o = Expression.Convert(property, typeof(object));
+                var lambda = Expression.Lambda<Func<TModel, object>>(o, parameter);
+
+                //utilisation lambda
+                return query.OrderByDescending(lambda);
             }
             else
                 return (IOrderedQueryable<TModel>)query;
